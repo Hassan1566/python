e6a4325b1387eaf56393dfb4922f5e5562ed8d30
Hassan1566 <sh456700992sj@gmail.com>
2025-08-28 09:14:26 +0500

    Add meeting_reminder.sh to create a reminder for meetings

---
+++ Meeting_info/meeting_reminder.sh
@@ -0,0 +1,5 @@
+meeting_info=$ (Zenity --forms / --text "Add reminder information" / --add-entry "Meeting Title" / --add-entry "Meeting Date" / --add-entry "Emails" / 2>/dev/null)
+
+if [[ -n "$meeting_info" ]]; then
+  python3 send_reminder.py "$meeting_info"
+fi
